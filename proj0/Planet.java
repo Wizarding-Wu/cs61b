07ca9d578460f665305a21b8c0fe60d8f2f4ac84
Niannian <niannian@pku.edu.cn>
2022-07-19 23:41:53 +0800

    change G to be private

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -3,7 +3,7 @@ public class Planet {
     public double mass;
     public String imgFileName;
 
-    public static double G = 6.67e-11;
+    static double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV, 
                  double yV, double m, String img) {
