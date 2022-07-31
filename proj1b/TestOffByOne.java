3d7f94762b12ecc6db5beb2ddf8ff0f992d5c91a
Niannian <niannian@pku.edu.cn>
2022-07-31 22:29:18 +0800

    add more tests in TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -9,10 +9,19 @@ public class TestOffByOne {
     public void testOffByOne() {
         OffByOne obo = new OffByOne();
         assertTrue(obo.equalChars('a', 'b'));
+        assertTrue(obo.equalChars('b', 'a'));
+
         assertTrue(obo.equalChars('r', 'q'));
+        assertTrue(obo.equalChars('q', 'r'));
 
         assertFalse(obo.equalChars('a', 'e'));
         assertFalse(obo.equalChars('z', 'a'));
         assertFalse(obo.equalChars('a', 'a'));
+
+        assertTrue(obo.equalChars('&', '%'));
+        assertTrue(obo.equalChars('%', '&'));
+
+        assertTrue(obo.equalChars('A', 'B'));
+        assertTrue(obo.equalChars('G', 'F'));
     }
 }
